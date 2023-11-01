2ac24c7ea54d14d27cc408ef91c6a3c7107cc523
DanielBackEnd <danielgithubb@gmail.com>
2023-11-01 11:52:20 +0100

    set connection with datebase

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRouter from './routes/UserRouter.js';
 import { notFound, errorHandler } from './middleware/customErrorMiddleware.js';
+import connectDB from './utils/db.js';
 
 dotenv.config();
 
 const port = process.env.PORT || 8000;
+
+connectDB();
 
 const app = express();
 
