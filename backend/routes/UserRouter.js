9e48a4a2ce64ad88450b0afe39cdedc57f488825
DanielBackEnd <danielgithubb@gmail.com>
2023-11-02 18:37:13 +0100

    added auth middleware to protected routes

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -6,13 +6,14 @@ import {
   getUserProfile,
   updateUserProfile,
 } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/login', loginUser);
 userRouter.post('/register', registerUser);
 userRouter.post('/logout', logoutUser);
-userRouter.get('/profile', getUserProfile);
-userRouter.put('/profile', updateUserProfile);
+userRouter.get('/profile', protect, getUserProfile);
+userRouter.put('/profile', protect, updateUserProfile);
 
 export default userRouter;
